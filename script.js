aa43b2c5f8ef872fbcdcd5f3b21725a36165b39c
Tom Mwambingu <[tomchom30@gmail.com]>
2025-10-31 16:40:00 +0300

    Implemented the js functionality for smooth flow

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const computerPoints = document.querySelector(".computerPoints");
 const playerPoints = document.querySelector(".playerPoints");
 const options = document.querySelectorAll(".options button");
 
+
 options.forEach((option) => {
   option.addEventListener("click", () => {
     computer.classList.add("shakeComputer");
